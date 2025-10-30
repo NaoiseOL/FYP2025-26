e20a66cbb0deabafbfe34022d1834e3bb5ff3bcd
NaoiseOL <naoiseol123@gmail.com>
2025-10-30 20:05:32 +0000

    Edited main to utilise use efficientnet instead of mobilenet

--- app/main.py
+++ app/main.py
@@ -1,23 +1,20 @@
 import tensorflow as tf
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+from tensorflow.keras.applications import EfficientNetV2B0
+from tensorflow.keras.applications.efficientnet_v2 import preprocess_input
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
-# Load the pre-trained model
-model = MobileNetV2(weights='imagenet')
+model = EfficientNetV2B0(weights='imagenet')
 
 def classify_image(image_path):
-    # Load and preprocess the image
     img = image.load_img(image_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
 
-    # Classify the image
     preds = model.predict(x)
     return preds
 
-# Classify a new image
-image_path = '001.jpg'
+image_path='catdog.jpg'
 preds = classify_image(image_path)
-print('Predicted:', tf.keras.applications.mobilenet_v2.decode_predictions(preds, top=3)[0])
+print("Predicted: ", tf.keras.applications.efficientnet_v2.decode_predictions(preds, top=3)[0])
