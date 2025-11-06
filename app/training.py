2e5f7c640e58d5a2b4f693721241faffbdd2efb6
NaoiseOL <naoiseol123@gmail.com>
2025-11-06 22:39:06 +0000

    Added plots to visualise results

--- app/training.py
+++ app/training.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import keras
 from keras import layers
 from keras.applications import EfficientNetV2B0
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 IMG_SIZE = 224
 BATCH_SIZE = 64
@@ -37,6 +39,47 @@ model = tf.keras.Sequential([
     layers.Dropout(0.2),
     layers.Dense(NUM_CLASSES, activation='softmax')
 ])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+history = model.fit(
+    ds_train,
+    validation_data = ds_test,
+    epochs=10
+)
+
 model.fit(ds_train, validation_data=ds_test, epochs=10)
+
+plt.figure(figsize=(12, 5))
+
+plt.subplot(1,2,1)
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Val Loss')
+plt.title('Loss over Epochs')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Val Accuracy')
+plt.title('Accuracy over Epochs')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+y_true = []
+y_pred = []
+
+for images, labels in ds_test.unbatch():
+    preds = model.predict(tf.expand_dims(images, axis=0), verbose=0)
+    y_true.append(labels.numpy())
+    y_pred.append(np.argmax(preds))
+
+cm = confusion_matrix(y_true, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=ds_test.class_names)
+disp.plot(cmap=plt.cm.Blues)
+plt.title("Confusion Matrix")
+plt.show()
