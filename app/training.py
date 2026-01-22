3595a76060f8c2f881d282d7b9df0e2e7f03aa74
NAOISE O LOUGHLIN - STUDENT <g00385498@atu.ie>
2026-01-22 12:24:57 +0000

    Added MHSA integration for testing

--- app/training.py
+++ app/training.py
@@ -30,26 +30,46 @@ ds_test = tf.keras.utils.image_dataset_from_directory(
 class_names = ds_train.class_names
 NUM_CLASSES = len(class_names)
 
-base_model= EfficientNetV2B1(include_top=False, weights='imagenet', input_shape=(IMG_SIZE, IMG_SIZE, 3))
+base_model= EfficientNetV2B1(
+    include_top=False, 
+    weights='imagenet', 
+    input_shape=(IMG_SIZE, IMG_SIZE, 3)
+    )
 base_model.trainable = False
 
-model = tf.keras.Sequential([
-    base_model,
-    layers.GlobalAveragePooling2D(),
-    layers.Dropout(0.2),
-    layers.Dense(NUM_CLASSES, activation='softmax')
-])
+inputs = keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
 
-def unfreeze_model(model):
-    for layer in model.layers[-20:]:
+x = base_model(inputs, training=False)
+
+h, w, c = base_model.output_shape[1:]
+x = layers.Reshape((h * w, c))(x)
+
+x = layers.MultiHeadAttention(
+    num_heads=4,
+    key_dim=c
+)(x, x)
+
+x = layers.LayerNormalization()(x)
+x = layers.GlobalAveragePooling1D()(x)
+x = layers.Dropout(0.2)(x)
+outputs = layers.Dense(NUM_CLASSES, activation="softmax")(x)
+
+model = keras.Model(inputs, outputs)
+
+def unfreeze_model(base_model, num_layers=20):
+    for layer in base_model.layers[-num_layers:]:
         if not isinstance(layer, layers.BatchNormalization):
             layer.trainable = True
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(
+    optimizer='adam', 
+    loss='sparse_categorical_crossentropy', 
+    metrics=['accuracy']
+)
 
-unfreeze_model(model)
+unfreeze_model(base_model, num_layers=20)
 
-history = model.fit(ds_train, validation_data=ds_test, epochs=20, class_weight={0:2.0, 1:1.0})
+history = model.fit(ds_train, validation_data=ds_test, epochs=5, class_weight={0:2.0, 1:1.0})
 
 os.makedirs("model", exist_ok=True)
 model.save("model/pixelProbeB1_CIFAKE.keras")
