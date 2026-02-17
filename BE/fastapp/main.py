e856dc98042d713fce23ffa6e1eb37f687f52f57
NAOISE O LOUGHLIN - STUDENT <g00385498@atu.ie>
2026-02-17 15:41:23 +0000

    Created basic file upload/save endpoint

--- BE/fastapp/main.py
+++ BE/fastapp/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+import os
+import shutil
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-@app.get("/hello")
-def hello():
-    return {"message": "Hello, World"}
+@app.post("/uploadfile")
+async def create_upload_file(file: UploadFile):
+    try:
+        file_path = f"BE/uploads/{file.filename}"
+        with open(file_path, "wb") as f:
+            f.write(file.file.read())
+            return {"message": "File saved successfully"}
+    except Exception as e:
+        return {"message": e.args}
